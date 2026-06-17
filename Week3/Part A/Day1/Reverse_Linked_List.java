39c36a318738db80b597055db3878da74a15ecb2
ans-hiii <anshichaudhary426@gmail.com>
2026-06-17 20:18:01 +0530

    Create Reverse_Linked_List.java

---
+++ Week3/Part A/Day1/Reverse_Linked_List.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public ListNode reverseList(ListNode head) {
+
+        ListNode previousNode = null;
+        ListNode currentNode = head;
+
+        while (currentNode != null) {
+
+            ListNode nextNode = currentNode.next;
+
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+
+        return previousNode;
+    }
+}
