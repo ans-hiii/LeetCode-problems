1a141dc73fcc68cc24cb053851a366fdfe3feaa7
ans-hiii <anshichaudhary426@gmail.com>
2026-06-17 20:16:15 +0530

    Create Linked_List_Cycle.java

---
+++ Week3/Part A/Day1/Linked_List_Cycle.java
@@ -0,0 +1,24 @@
+public class Solution {
+
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+
+        while (fastPointer != null && fastPointer.next != null) {
+
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+
+            if (slowPointer == fastPointer) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
